1fe391d6734979ed65817b32840ff48646ea48b2
Andrei Muntean <andrei.muntean.dev@gmail.com>
2016-11-14 22:24:06 +0000

    Updated epsilon annealing mechanism

--- pong.py
+++ pong.py
@@ -26,22 +26,40 @@ parser.add_argument('--num_episodes',
                     type=int,
                     default=10000)
 
+parser.add_argument('--start_epsilon',
+                    help='initial value for epsilon (exploration chance)',
+                    type=float,
+                    default=1)
+
+parser.add_argument('--end_epsilon',
+                    help='final value for epsilon (exploration chance)',
+                    type=float,
+                    default=0.1)
+
+parser.add_argument('--anneal_duration',
+                    help='number of episodes to decrease epsilon from start_epsilon to end_epsilon',
+                    type=int,
+                    default=100)
+
 args = parser.parse_args()
 
 load_path = args.load_path
 save_path = args.save_path
 save_interval = args.save_interval
 num_episodes = args.num_episodes
+start_epsilon = args.start_epsilon
+end_epsilon = args.end_epsilon
+anneal_duration = args.anneal_duration
 
 batch_size = 32
 wait_before_training = 5000
-train_interval = 1
+train_interval = 4
 discount = 0.99
 
 env = environment.AtariWrapper(gym.make('Pong-v0'),
                                action_space=[0, 2, 3], # 'NONE', 'UP' and 'DOWN'.
                                observations_per_state=4,
-                               replay_memory_capacity=20000)
+                               replay_memory_capacity=24000)
 
 epsilon_history = []
 reward_history = []
@@ -55,15 +73,18 @@ with tf.Session() as sess:
         saver.restore(sess, load_path)
         print('Model restored.')
 
+    epsilon = start_epsilon
     t = 0
+
     for i in range(1, num_episodes + 1):
-        # Epsilon anneals from 1 to 0.05.
-        epsilon = max(min(20 / i, 1), 0.05)
+        # Epsilon anneals from start_epsilon to end_epsilon.
+        if epsilon > end_epsilon:
+            epsilon -= (start_epsilon - end_epsilon) / anneal_duration
+        
         episode_reward = 0
 
         while not env.done:
             t += 1
-            env.render()
 
             # Occasionally train.
             if t > wait_before_training and t % train_interval == 0:
@@ -75,9 +96,6 @@ with tf.Session() as sess:
                 next_states = np.stack(experiences[:, 3], axis=0)
                 done = np.stack(experiences[:, 4], axis=0)
 
-                # Estimate action values.
-                Q = network.eval_Q(states, actions_i)
-
                 # Determine the true action values.
                 #
                 #                    { r, if next state is terminal
@@ -85,8 +103,8 @@ with tf.Session() as sess:
                 #                    { r + discount * max(Q(next state, <any action>)), otherwise
                 Q_ = rewards + ~done * discount * network.eval_optimal_action_value(next_states)
                 
-                # Estimate error and update weights.
-                network.train(states, actions_i, Q_)
+                # Estimate action values, measure errors and update weights.
+                network.train(states, actions_i, Q_, learning_rate=1e-4)
 
             # Occasionally try a random action (explore).
             if np.random.rand() < epsilon:
