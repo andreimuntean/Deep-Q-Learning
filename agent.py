977515fbd2753e4815ea0adec8aadadd87aef685
Andrei Muntean <andrei.muntean.dev@gmail.com>
2016-11-16 06:52:12 +0000

    Added more flags & refactored some code into a separate Agent class

---
+++ agent.py
@@ -0,0 +1,126 @@
+"""Defines an agent that learns to play Atari games using deep Q-learning.
+
+Heavily influenced by DeepMind's seminal paper 'Playing Atari with Deep Reinforcement Learning'
+(Mnih et al., 2013).
+"""
+
+import dqn
+import numpy as np
+import tensorflow as tf
+
+
+class Agent():
+    def __init__(self,
+                 sess,
+                 env,
+                 start_epsilon=1,
+                 end_epsilon=0,
+                 anneal_duration=500,
+                 wait_before_training=5000,
+                 train_interval=100,
+                 batch_size=1024,
+                 discount=0.99):
+        """An agent that learns to play Atari games using deep Q-learning.
+
+        Args:
+            sess: The associated TensorFlow session.
+            env: An AtariWrapper object (see 'environment.py') that wraps over an OpenAI Gym Atari
+                environment.
+            start_epsilon: Initial value for epsilon (exploration chance).
+            end_epsilon: Final value for epsilon (exploration chance).
+            anneal_duration: Number of episodes needed to decrease epsilon from start_epsilon to
+                end_epsilon.
+            wait_before_training: Number of experiences to accumulate before training starts.
+            train_interval: Number of experiences to accumulate before another round of training
+                starts.
+            batch_size: Number of experiences sampled and trained on at once.
+            discount: Discount factor for future rewards.
+        """
+        
+        self.sess = sess
+        self.env = env
+        self.dqn = dqn.DeepQNetwork(sess, len(env.action_space), env.state_space)
+        self.start_epsilon = start_epsilon
+        self.end_epsilon = end_epsilon
+        self.anneal_duration = anneal_duration
+        self.wait_before_training = wait_before_training
+        self.train_interval = train_interval
+        self.batch_size = batch_size
+        self.discount = discount
+        self.t = 0
+        self.episodes_played = 0
+
+    def train(self, render=True, learning_rate=1e-6):
+        if self.env.done:
+            self.env.restart()
+
+        self.episodes_played += 1
+        episode_reward = 0
+        epsilon = self._get_epsilon()
+
+        while not self.env.done:
+            self.t += 1
+            
+            if render:
+                self.env.render()
+
+            # Occasionally train.
+            if self.t > self.wait_before_training and self.t % self.train_interval == 0:
+                # These operations might be confusing if you forget that they're vectorized.
+                experiences = self.env.sample_experiences(self.batch_size)
+                states = np.stack(experiences[:, 0], axis=0)
+                actions_i = np.stack([self.env.action_space.index(a) for a in experiences[:, 1]],
+                                     axis=0)
+                rewards = np.stack(experiences[:, 2], axis=0)
+                next_states = np.stack(experiences[:, 3], axis=0)
+                done = np.stack(experiences[:, 4], axis=0)
+
+                # Determine the true action values.
+                #
+                #                    { r, if next state is terminal
+                # Q(state, action) = {
+                #                    { r + discount * max(Q(next state, <any action>)), otherwise
+                Q_ = rewards + ~done * self.discount * self.dqn.eval_optimal_action_value(next_states)
+                
+                # Estimate action values, measure errors and update weights.
+                self.dqn.train(states, actions_i, Q_, learning_rate=learning_rate)
+
+            # Occasionally try a random action (explore).
+            if np.random.rand() < epsilon:
+                action = self.env.sample_action()
+            else:
+                state = np.expand_dims(self.env.get_state(), axis=0)
+                action = self.env.action_space[self.dqn.eval_optimal_action(state)[0]]
+
+            episode_reward += self.env.step(action)
+
+        return episode_reward
+
+    def play(self, render=True):
+        if self.env.done:
+            self.env.restart()
+
+        episode_reward = 0
+
+        while not self.env.done:
+            if render:
+                self.env.render()
+
+            state = np.expand_dims(self.env.get_state(), axis=0)
+            action = self.env.action_space[self.dqn.eval_optimal_action(state)[0]]
+            episode_reward += self.env.step(action)
+
+        return episode_reward
+
+    def _get_epsilon(self):
+        """Gets the epsilon value (exploration chance) for the current episode."""
+
+        # Epsilon anneals from start_epsilon to end_epsilon.
+        if self.anneal_duration == 0:
+            return self.end_epsilon
+
+        start_weight = (self.anneal_duration - self.episodes_played) / self.anneal_duration
+        end_weight = self.episodes_played / self.anneal_duration
+
+        return max(self.end_epsilon,
+                   self.start_epsilon * start_weight + self.end_epsilon * end_weight)
--- pong.py
+++ pong.py
@@ -1,146 +1,106 @@
+import agent
 import argparse
 import datetime
-import dqn
 import environment
 import gym
-import numpy as np
 import tensorflow as tf
-
-from matplotlib import pyplot as plt
 
 parser = argparse.ArgumentParser(description='Train an agent to play Pong.')
 
-parser.add_argument('--load_path', help='loads a trained model from the specified path')
+parser.add_argument('--load_path',
+                    metavar='PATH',
+                    help='loads a trained model from the specified path')
 
 parser.add_argument('--save_path',
+                    metavar='PATH',
                     help='saves the model at the specified path',
                     default='checkpoints/tmp/model.ckpt')
 
 parser.add_argument('--save_interval',
+                    metavar='TIME_STEPS',
                     help='time step interval at which to save the model',
                     type=int,
                     default=30000)
 
 parser.add_argument('--num_episodes',
+                    metavar='EPISODES',
                     help='number of episodes that will be played',
                     type=int,
-                    default=10000)
+                    default=1000)
 
 parser.add_argument('--start_epsilon',
+                    metavar='PERCENTAGE',
                     help='initial value for epsilon (exploration chance)',
                     type=float,
                     default=1)
 
 parser.add_argument('--end_epsilon',
+                    metavar='PERCENTAGE',
                     help='final value for epsilon (exploration chance)',
                     type=float,
                     default=0)
 
 parser.add_argument('--anneal_duration',
+                    metavar='EPISODES',
                     help='number of episodes to decrease epsilon from start_epsilon to end_epsilon',
                     type=int,
-                    default=250)
+                    default=300)
+
+parser.add_argument('--wait_before_training',
+                    metavar='TIME_STEPS',
+                    help='number of experiences to accumulate before training starts',
+                    type=int,
+                    default=5000)
+
+parser.add_argument('--train_interval',
+                    metavar='TIME_STEPS',
+                    help='number of experiences to accumulate before next round of training starts',
+                    type=int,
+                    default=100)
+
+parser.add_argument('--batch_size',
+                    metavar='N',
+                    help='number of experiences sampled and trained on at once',
+                    type=int,
+                    default=1024)
+
+parser.add_argument('--discount',
+                    metavar='PERCENTAGE',
+                    help='discount factor for future rewards',
+                    type=float,
+                    default=0.99)
 
 args = parser.parse_args()
-
-load_path = args.load_path # Loads a trained model from the specified path.
-save_path = args.save_path # Saves the model at the specified path.
-save_interval = args.save_interval # Time step interval at which to save the model.
-num_episodes = args.num_episodes # Number of episodes that will be played.
-start_epsilon = args.start_epsilon # Initial value for epsilon (exploration chance).
-end_epsilon = args.end_epsilon # Final value for epsilon (exploration chance).
-anneal_duration = args.anneal_duration # Number of episodes needed to decrease epsilon from
-                                       # start_epsilon to end_epsilon.
-
-batch_size = 1024 # Number of experiences sampled and trained on at once.
-wait_before_training = 5000 # Number of experiences to accumulate before training starts.
-train_interval = 100 # Number of experiences to accumulate before another round of training starts.
-discount = 0.99 # Discount factor for future rewards.
-
 env = environment.AtariWrapper(gym.make('Pong-v0'),
                                action_space=[0, 2, 3], # 'NONE', 'UP' and 'DOWN'.
                                observations_per_state=4,
                                replay_memory_capacity=24000)
 
-epsilon_history = []
-reward_history = []
-
 with tf.Session() as sess:
-    network = dqn.DeepQNetwork(sess, len(env.action_space), env.state_space)
+    learner = agent.Agent(sess,
+                          env,
+                          args.start_epsilon,
+                          args.end_epsilon,
+                          args.anneal_duration,
+                          args.wait_before_training,
+                          args.train_interval,
+                          args.batch_size,
+                          args.discount)
+
     sess.run(tf.initialize_all_variables())
     saver = tf.train.Saver()
+    save_next = args.save_interval
 
-    if load_path:
-        saver.restore(sess, load_path)
+    if args.load_path:
+        saver.restore(sess, args.load_path)
         print('Model restored.')
 
-    epsilon = start_epsilon
-    t = 0
+    for i in range(1, args.num_episodes + 1):
+        reward = learner.train(render=True)
+        epsilon = learner._get_epsilon()
+        print('Episode: {}  Reward: {}  Epsilon: {}'.format(i, reward, epsilon))
 
-    for i in range(num_episodes):
-        # Epsilon anneals from start_epsilon to end_epsilon.
-        epsilon = max(end_epsilon,
-                      (start_epsilon * (anneal_duration - i) + end_epsilon * i) / anneal_duration)
-        episode_reward = 0
-        episode_loss = []
-
-        while not env.done:
-            t += 1
-            env.render()
-
-            # Occasionally train.
-            if t > wait_before_training and t % train_interval == 0:
-                # These operations might be confusing if you forget that they're vectorized.
-                experiences = env.sample_experiences(batch_size)
-                states = np.stack(experiences[:, 0], axis=0)
-                actions_i = np.stack([env.action_space.index(a) for a in experiences[:, 1]], axis=0)
-                rewards = np.stack(experiences[:, 2], axis=0)
-                next_states = np.stack(experiences[:, 3], axis=0)
-                done = np.stack(experiences[:, 4], axis=0)
-
-                # Determine the true action values.
-                #
-                #                    { r, if next state is terminal
-                # Q(state, action) = {
-                #                    { r + discount * max(Q(next state, <any action>)), otherwise
-                Q_ = rewards + ~done * discount * network.eval_optimal_action_value(next_states)
-                
-                # Estimate action values, measure errors and update weights.
-                network.train(states, actions_i, Q_, learning_rate=1e-6)
-
-            # Occasionally try a random action (explore).
-            if np.random.rand() < epsilon:
-                action = env.sample_action()
-            else:
-                state = np.expand_dims(env.get_state(), axis=0)
-                action = env.action_space[network.eval_optimal_action(state)[0]]
-
-            episode_reward += env.step(action)
-
-            if save_path and t % save_interval == 0:
-                saver.save(sess, save_path)
-                print('[{}] Saved model at "{}".'.format(datetime.datetime.now(), save_path))
-
-        epsilon_history.append(epsilon)
-        reward_history.append(episode_reward)
-        print('Episode: {}  Reward: {}  Epsilon: {}'.format(i + 1, episode_reward, epsilon))
-        env.restart()
-
-if save_path:
-    saver.save(sess, save_path)
-    print('[{}] Saved model at "{}".'.format(datetime.datetime.now(), save_path))
-
-print('Total timesteps:', t)
-
-plt.subplot(211)
-plt.ylabel('Reward')
-plt.xlabel('Episode')
-plt.plot(reward_history)
-
-plt.subplot(212)
-plt.ylabel('Explore / Exploit')
-plt.xlabel('Episode')
-plt.plot(epsilon_history)
-
-plt.tight_layout()
-plt.show()
+        if args.save_path and learner.t > save_next or i == args.num_episodes:
+            saver.save(sess, args.save_path)
+            print('[{}] Saved model at "{}".'.format(datetime.datetime.now(), args.save_path))
+            save_next += args.save_interval
