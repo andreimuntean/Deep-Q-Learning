877ce8096a3a1de506ae4e00f05002a1f538c0ad
Ubuntu <geanina@DeepLearningVM.jv0thpg2bkrejkvpf5qwc4trea.bx.internal.cloudapp.net>
2017-01-11 22:12:00 +0000

    DQN now uses a dueling network architecture

--- dqn.py
+++ dqn.py
@@ -6,23 +6,8 @@ Heavily influenced by DeepMind's seminal paper 'Playing Atari with Deep Reinforc
 """
 
 import math
+import numpy as np
 import tensorflow as tf
-
-
-def _create_fc_weights(shape):
-    # Use Xavier initialization.
-    minval = -math.sqrt(6.0 / (shape[0] + shape[1]))
-    maxval = math.sqrt(6.0 / (shape[0] + shape[1]))
-    value = tf.random_uniform(shape, minval, maxval)
-    return tf.Variable(value, name='Weights')
-
-
-def _create_conv2d_weights(shape):
-    # Use Xavier initialization.
-    minval = -math.sqrt(6.0 / (shape[0] * shape[1] * shape[2] + shape[3]))
-    maxval = math.sqrt(6.0 / (shape[0] * shape[1] * shape[2] + shape[3]))
-    value = tf.random_uniform(shape, minval, maxval)
-    return tf.Variable(value, name='Weights')
 
 
 def _create_bias(shape):
@@ -31,8 +16,35 @@ def _create_bias(shape):
     return tf.Variable(value, name='Bias')
 
 
-def _create_conv2d(x, W, stride):
-    return tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding='VALID')
+def _create_weights(shape):
+    # Use Xavier initialization.
+    maxval = math.sqrt(6.0 / (np.prod(shape[:-1]) + shape[-1]))
+    minval = -maxval
+    value = tf.random_uniform(shape, minval, maxval)
+    return tf.Variable(value, name='Weights')
+
+
+def _create_conv2d_layer(x, shape, stride, activation_fn):
+    if len(shape) != 4:
+        raise ValueError('Shape "{}" is invalid. Must have length 4.'.format(shape))
+
+    W = _create_weights(shape)
+    b = _create_bias([shape[3]])
+
+    return activation_fn(tf.nn.conv2d(x, W, [1, stride, stride, 1], 'VALID') + b)
+
+
+def _create_fc_layer(x, shape, activation_fn=None):
+    if len(shape) != 2:
+        raise ValueError('Shape "{}" is invalid. Must have length 2.'.format(shape))
+
+    W = _create_weights(shape)
+    b = _create_bias([shape[1]])
+
+    if activation_fn is None:
+        return tf.matmul(x, W) + b
+
+    return activation_fn(tf.matmul(x, W) + b)
 
 
 class DeepQNetwork():
@@ -53,54 +65,53 @@ class DeepQNetwork():
         self.x = tf.placeholder(tf.float32, [None, width, height, depth], name='Input_States')
 
         with tf.name_scope('Convolutional_Layer_1'):
-            W_conv1 = _create_conv2d_weights([8, 8, depth, 32])
-            b_conv1 = _create_bias([32])
-            h_conv1 = tf.nn.relu(_create_conv2d(self.x, W_conv1, stride=4) + b_conv1)
+            h_conv1 = _create_conv2d_layer(self.x, [8, 8, depth, 32], 4, tf.nn.relu)
 
         with tf.name_scope('Convolutional_Layer_2'):
-            W_conv2 = _create_conv2d_weights([4, 4, 32, 64])
-            b_conv2 = _create_bias([64])
-            h_conv2 = tf.nn.relu(_create_conv2d(h_conv1, W_conv2, stride=2) + b_conv2)
+            h_conv2 = _create_conv2d_layer(h_conv1, [4, 4, 32, 64], 2, tf.nn.relu)
 
         with tf.name_scope('Convolutional_Layer_3'):
-            W_conv3 = _create_conv2d_weights([3, 3, 64, 64])
-            b_conv3 = _create_bias([64])
-            h_conv3 = tf.nn.relu(_create_conv2d(h_conv2, W_conv3, stride=1) + b_conv3)
+            h_conv3 = _create_conv2d_layer(h_conv2, [3, 3, 64, 64], 1, tf.nn.relu)
 
-        # Flatten the output to feed it into the fully connected layer.
+        # Flatten the output to feed it into fully connected layers.
         post_conv_height = math.ceil((math.ceil((height - 7) / 4) - 3) / 2) - 2
         post_conv_width = math.ceil((math.ceil((width - 7) / 4) - 3) / 2) - 2
         num_params = post_conv_height * post_conv_width * 64
         h_flat = tf.reshape(h_conv3, [-1, num_params])
 
-        with tf.name_scope('Fully_Connected_Layer'):
-            W_fc = _create_fc_weights([num_params, 512])
-            b_fc = _create_bias([512])
-            h_fc = tf.nn.relu(tf.matmul(h_flat, W_fc) + b_fc)
-
-        # Implement dropout.
+        # Diverge into two streams: the first stream learns the advantage of each action and the
+        # second stream estimates state values.
         self.keep_prob = tf.placeholder(tf.float32, name='Keep_Prob')
-        h_fc_drop = tf.nn.dropout(h_fc, self.keep_prob)
 
-        with tf.name_scope('Output'):
-            W_output = _create_fc_weights([512, num_actions])
-            b_output = _create_bias([num_actions])
-            h_output = tf.matmul(h_fc_drop, W_output) + b_output
+        with tf.name_scope('Advantage_Stream'):
+            h_advantage_fc = _create_fc_layer(h_flat, [num_params, 512], tf.nn.relu)
+            h_advantage_drop = tf.nn.dropout(h_advantage_fc, self.keep_prob)
+            advantage = _create_fc_layer(h_advantage_drop, [512, num_actions])
+
+        with tf.name_scope('State_Value_Stream'):
+            h_state_value_fc = _create_fc_layer(h_flat, [num_params, 512], tf.nn.relu)
+            h_state_value_drop = tf.nn.dropout(h_state_value_fc, self.keep_prob)
+            state_value = _create_fc_layer(h_state_value_drop, [512, 1])
+
+        # Connect streams and estimate action values (Q). To improve training stability as suggested
+        # by Wang et al., 2015, Q = state value + advantage - mean(advantage).
+        self.Q = state_value + advantage - tf.reduce_mean(advantage, 1, keep_dims=True)
 
         # Estimate the optimal action and its expected value.
-        self.optimal_action = tf.argmax(h_output, 1, name='Optimal_Action')
-        self.optimal_action_value = tf.reduce_max(h_output, 1, name='Optimal_Action_Value')
+        self.optimal_action = tf.argmax(self.Q, 1, name='Optimal_Action')
+        self.optimal_action_value = tf.reduce_max(self.Q, 1,)
 
         # Estimate the value of the specified action.
         self.action = tf.placeholder(tf.uint8, name='Action')
         one_hot_action = tf.one_hot(self.action, num_actions)
-        self.Q = tf.reduce_sum(h_output * one_hot_action, 1, name='Estimated_Action_Value')
+        self.estimated_action_value = tf.reduce_sum(self.Q * one_hot_action, 1)
 
         # Compare with the observed action value.
-        self.Q_ = tf.placeholder(tf.float32, [None], name='Observed_Action_Value')
-        self.loss = tf.reduce_mean(tf.square(self.Q - self.Q_))
+        self.observed_action_value = tf.placeholder(
+            tf.float32, [None], name='Observed_Action_Value')
+        loss = tf.reduce_mean(tf.square(self.estimated_action_value - self.observed_action_value))
         self.learning_rate = tf.placeholder(tf.float32, name='Learning_Rate')
-        self.train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
+        self.train_step = tf.train.AdamOptimizer(self.learning_rate).minimize(loss)
 
     def eval_optimal_action(self, state):
         """Estimates the optimal action for the specified state.
@@ -129,17 +140,17 @@ class DeepQNetwork():
             action: An action. Can be batched into multiple actions.
         """
 
-        return self.sess.run(self.Q, feed_dict={self.x: state,
-                                                self.action: action,
-                                                self.keep_prob: 1})
+        return self.sess.run(self.estimated_action_value, feed_dict={self.x: state,
+                                                                     self.action: action,
+                                                                     self.keep_prob: 1})
 
-    def train(self, state, action, Q_, learning_rate, dropout_prob):
+    def train(self, state, action, observed_action_value, learning_rate, dropout_prob):
         """Learns by performing one step of gradient descent.
 
         Args:
             state: A state. Can be batched into multiple states.
             action: An action. Can be batched into multiple actions.
-            Q_: An observed action value.
+            observed_action_value: An observed action value (the ground truth).
             learning_rate: The speed with which the network learns from new examples.
             dropout_prob: Likelihood of individual neurons from the fully connected layer becoming
                 inactive.
@@ -147,6 +158,7 @@ class DeepQNetwork():
 
         self.sess.run(self.train_step, feed_dict={self.x: state,
                                                   self.action: action,
-                                                  self.Q_: Q_,
+                                                  self.observed_action_value: observed_action_value,
                                                   self.learning_rate: learning_rate,
                                                   self.keep_prob: 1 - dropout_prob})
+
